7f0376f3766c9634564070eb77b3accaa9eec29f
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-18 07:42:29 +0530

    added: first name to the user registration

---
+++ src/com/bridgelabz/RegexMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class RegexMain {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to User Registration Problem");
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.firstName();
+    }
+}
+
